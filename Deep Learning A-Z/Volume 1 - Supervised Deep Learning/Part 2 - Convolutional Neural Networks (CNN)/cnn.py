6716f68caea9b347369b9e6cc5829ee4b3a5454b
Gabor Schmied <gabor.schmied@cognex.com>
2017-08-31 12:16:48 +0200

    fixes in cnn.py at image fitting

--- Deep Learning A-Z/Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/cnn.py
+++ Deep Learning A-Z/Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/cnn.py
@@ -45,18 +45,18 @@ train_datagen = ImageDataGenerator(rescale=1./255,
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
-traning_set = train_datagen.flow_from_directory('dataset/training_set',
+training_set = train_datagen.flow_from_directory('dataset/training_set',
                                                 target_size=(64, 64),
                                                 batch_size=32,
                                                 class_mode='binary')
 
-validation_generator = test_datagen.flow_from_directory('dataset/test_set',
-                                                        target_size=(64, 64),
-                                                        batch_size=32,
-                                                        class_mode='binary')
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size=(64, 64),
+                                            batch_size=32,
+                                            class_mode='binary')
 
-classifier.fit_generator(train_datagen,
-                         steps_per_epoch=8000,
+classifier.fit_generator(training_set,
+                         steps_per_epoch=(8000 / training_set.batch_size),
                          epochs=25,
-                         validation_data=validation_generator,
-                         validation_steps=2000)
+                         validation_data=test_set,
+                         validation_steps=(2000 / training_set.batch_size))
