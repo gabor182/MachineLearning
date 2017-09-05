ebd7704e56ed377e0877ca5aed08919fbecf08ec
gabor182 <schmied.gabor@gmail.com>
2017-09-05 16:27:14 +0200

    boston network update for best mse and mea score

--- Deep Learning with Python/Chapter 3/boston_housing.py
+++ Deep Learning with Python/Chapter 3/boston_housing.py
@@ -59,13 +59,20 @@ for i in range(k):
     mae_history = history.history['val_mean_absolute_error']
     all_mae_histories.append(mae_history)
     
-average_mae_history = [np.mean([x[i] for x in all_mae_histories] for i in range(num_epochs))]
+average_mae_history = [np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
 
 # Plotting validation scores
 
 import matplotlib.pyplot as plt
 
 plt.plot(range(len(average_mae_history)), average_mae_history)
+plt.xlabel('Epochs')
+plt.ylabel('Validation MAE')
+plt.show()
+
+plt.clf()
+
+plt.plot(range(len(average_mae_history) - 10), average_mae_history[10:])
 plt.xlabel('Epochs')
 plt.ylabel('Validation MAE')
 plt.show()
